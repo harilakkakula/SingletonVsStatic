9ba192bd402c5bb29265e81de1e5816074027636
Harikrishna Lakkakula <harikrishna.lakkakula@sparinfosys.com>
2021-09-26 01:46:50 +0530

    Add project files.

---
+++ SingletonVsStatic/Convertor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonVsStatic
+{
+    public static class Convertor
+    {
+        public static double ToFahrenheit(double celcius)
+        {
+            return (celcius * 9 / 5) + 32;
+        }
+
+        public static double ToCelcius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
---
+++ SingletonVsStatic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SingletonVsStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double celcius = 37;double fahernheit = 98.6;
+            Console.WriteLine($"Value of {celcius} celcius to fahernheit is {Convertor.ToFahrenheit(celcius)}");
+            Console.WriteLine($"Value of {fahernheit} fahrenheit to celcius is {Convertor.ToCelcius(fahernheit)}");
+
+            Console.ReadLine();
+            
+
+            ///IMP ************************************************
+            /// When to use Single ton?
+            /// In Real Time application we are useing in below seanarios
+            /// Logging
+            /// Managing a Connection or a pool of the connections to DataBase
+            /// Print spooling
+            /// File
+            /// COnfiguration
+            /// Cache
+            /// Session based Shopping Cart
+
+        }
+    }
+}
